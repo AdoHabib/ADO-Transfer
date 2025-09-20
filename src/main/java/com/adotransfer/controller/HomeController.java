8a468cb84497b2294ce5ac28dac9ae5eed9372a8
ADO <mtoure@studenti.uninsubria.it>
2025-09-20 18:33:39 +0200

    versione_02.01

--- src/main/java/com/adotransfer/controller/HomeController.java
+++ src/main/java/com/adotransfer/controller/HomeController.java
@@ -8,30 +8,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
-public class RootController {
+public class HomeController {
 
     private final Environment environment;
 
-    public RootController(Environment environment) {
+    public HomeController(Environment environment) {
         this.environment = environment;
     }
 
     @GetMapping("/")
-    public Map<String, Object> root() {
+    public Map<String, Object> home() {
         Map<String, Object> info = new HashMap<>();
+        info.put("message", "Benvenuto in ADO Transfer!");
         info.put("application", "ADO Transfer");
         info.put("version", "1.0.0");
         info.put("profile", getActiveProfile());
         info.put("status", "UP");
+        info.put("instructions", "Vai a /api/ per accedere all'API");
         info.put("endpoints", Map.of(
+            "api_root", "/api/",
             "health", "/api/health/simple",
             "swagger", "/api/swagger-ui.html",
             "api-docs", "/api/v3/api-docs"
         ));
         return info;
     }
-
 
     private String getActiveProfile() {
         try {
