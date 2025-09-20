2e126199695b1da3faf62c3fa1c9f012b20f1913
ADO <mtoure@studenti.uninsubria.it>
2025-09-20 18:35:17 +0200

    versione_02.02

--- src/main/java/com/adotransfer/controller/RootController.java
+++ src/main/java/com/adotransfer/controller/RootController.java
@@ -2,6 +2,7 @@ package com.adotransfer.controller;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
